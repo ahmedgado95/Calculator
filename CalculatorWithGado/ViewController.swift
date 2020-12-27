ed43d2abd0f41ecd266812d84cc0c98c78ac0fc4
ahmedgado95 <ahmedgado2001@gmail.com>
2020-12-27 12:52:37 +0200

    Initial Commit

---
+++ CalculatorWithGado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorWithGado
+//
+//  Created by ahmed gado on 27/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
